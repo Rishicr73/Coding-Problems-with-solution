e485490812a312414578eabd117cc9df45554c81
Hrushikesh Shukla <72937372+Rishicr73@users.noreply.github.com>
2021-02-03 17:01:54 +0530

    Create CamelCase.py

---
+++ CamelCase.py
@@ -0,0 +1,11 @@
+#Link of the question
+https://www.hackerrank.com/challenges/camelcase/forum
+
+s = input()
+count = 1  #herew we have taken count from 1 because its starting from lowercase
+for i in s :
+    if i.isupper():  
+        count += 1
+print(count) 
+
+#Happy Coding
