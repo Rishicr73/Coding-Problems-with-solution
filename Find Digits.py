760a65a06c0c390eef99153d5fbb072c2c1c8952
Hrushikesh Shukla <72937372+Rishicr73@users.noreply.github.com>
2021-02-05 18:45:28 +0530

    Create Find Digits.py

---
+++ Find Digits.py
@@ -0,0 +1,10 @@
+t = int(input())
+for a0 in rnage(t):
+  n = int(input())
+  count = 0
+  for i in str(i):
+    if i != str(0) and n % int(i) == 0:
+      count += 1
+   print(count)   
+ #link of the question
+# https://www.hackerrank.com/challenges/find-digits/problem
