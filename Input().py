5a2cafd9bead13e7f9b6f7e2faef76c914d2338c
Hrushikesh Shukla <72937372+Rishicr73@users.noreply.github.com>
2021-02-07 23:06:56 +0530

    Create Input().py

---
+++ Input().py
@@ -0,0 +1,10 @@
+x , k = map(int , input().split())
+print(eval(input()) == k)
+
+
+#WHAT IS EVAL?
+#ANS : x = 2
+print eval ('x+1')
+IS JUST TAKE VALUE GIVEN AND EVALUATE IN THAT FORM
+
+#LINK: https://www.hackerrank.com/challenges/input/problem
