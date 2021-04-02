7b6388ecfadd97d2838d4caf821f1b3f6a52785e
Hrushikesh Shukla <72937372+Rishicr73@users.noreply.github.com>
2021-04-02 16:31:26 +0530

    Create Gemstones.py

---
+++ Gemstones.py
@@ -0,0 +1,33 @@
+#link : https://www.hackerrank.com/challenges/gem-stones/problem
+
+#code:
+!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the gemstones function below.
+def gemstones(arr):
+    t = set(arr[0])
+    for i in range(1 , len(arr)):
+        t = t.intersection(set(arr[i])) #Intersection:similar elements between them
+    return(len(t))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = input()
+        arr.append(arr_item)
+
+    result = gemstones(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
